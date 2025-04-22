3a548d3ee4bf8f1e62c0740b40137400c16f6e0c
Kristal-LID <savelii_iakovlev@icloud.com>
2025-04-22 22:30:45 +0500

    homework 8 security

---
+++ src/main/java/com/urfu/homework/springsecurity/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.urfu.homework.springsecurity;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true) // для поддержки @RolesAllowed
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/api").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> basic.realmName("Security Demo"))
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails support = User.builder()
+                .username("support")
+                .password(passwordEncoder.encode("support"))
+                .roles("SUPPORT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, support);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/urfu/homework/springsecurity/UserInfo.java
@@ -0,0 +1,14 @@
+package com.urfu.homework.springsecurity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserInfo {
+    private String username;
+    private List<String> roles;
+}
