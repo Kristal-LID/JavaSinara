9c84bed92426fe0c27f7f6fd5608e1f8806a20cd
Kristal-LID <savelii_iakovlev@icloud.com>
2025-04-30 00:00:10 +0500

    homework 10 Aspects

--- src/main/java/com/urfu/homework/listener/AsyncEvent.java
+++
@@ -1,4 +0,0 @@
-package com.urfu.homework.listener;
-
-public record AsyncEvent(String message) {
-}
--- src/main/java/com/urfu/homework/listener/RegularEvent.java
+++
@@ -1,4 +0,0 @@
-package com.urfu.homework.listener;
-
-public record RegularEvent(String message) {
-}
